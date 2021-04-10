454f59df1ad51586223d483d265e209f284cb494
DSWhiteHat <33768979+DSWhiteHat@users.noreply.github.com>
2021-04-10 13:03:44 -0400

    Average error

--- Empathy.py
+++ Empathy.py
@@ -4,27 +4,27 @@ from textblob import TextBlob
 from textblob.sentiments import NaiveBayesAnalyzer
 
 def empathy(prompt, response):
-    print(te.get_emotion(prompt))
-    print(te.get_emotion(response))
+    #print(te.get_emotion(prompt))
+    #print(te.get_emotion(response))
 
-    print(TextBlob(prompt, analyzer=NaiveBayesAnalyzer()).sentiment)
-    print(TextBlob(response, analyzer=NaiveBayesAnalyzer()).sentiment)
+    #print(TextBlob(prompt, analyzer=NaiveBayesAnalyzer()).sentiment)
+    #print(TextBlob(response, analyzer=NaiveBayesAnalyzer()).sentiment)
 
     # Gets just the emotion values for each statement.
     promptEmotions = te.get_emotion(prompt).values()
     responseEmotions = te.get_emotion(response).values()
 
     # Calculates the distance between the emotion vectors.
-    return 5 - ((sum(pow(float(a)-float(b),2) for a, b in zip(promptEmotions, responseEmotions)) / 2) ** (1/2)) * 4
+    return 5 - ((sum(pow(float(a)-float(b),2) for a, b in zip(promptEmotions, responseEmotions)) * 8) ** (1/2))
 
-def getConversations():
+def getConversations(n):
     train = pd.read_csv("train.csv")
     train = train.iloc[:, :7].dropna()
     selfeval = train['selfeval']
     train["empathy"] = selfeval.str.extract('\d\|\d\|\d_(\d)\|\d\|\d')
     train = train.dropna()
     conversations = train["conv_id"].value_counts()
-    conversations = conversations.iloc[:100]
+    conversations = conversations.iloc[:n]
     lsconv = []
     for ids in conversations.index:
         convo = train.loc[train["conv_id"] == ids]
@@ -41,12 +41,15 @@ def getConversations():
         lsconv.append([context, prompt + s1words, s2words, escore])
     return lsconv
 
-def testing():
-    conversations = getConversations()
+def testing(n):
+    conversations = getConversations(n)
+    error = 0
     for conversation in conversations:
         person1 = " ".join(conversation[1])
         person2 = " ".join(conversation[2])
         #print(person2)
         #print("Prompt Length: " + str(len(person1)))
         #print("Response Length: " + str(len(person2)))
-        print(empathy(person1, person2))
+        temp = conversation[3] - empathy(person1, person2)
+        error += abs(temp)
+    print(error / n)
