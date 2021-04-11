c7b1b80ced17893fb0d440d41dad1970e12e8691
parkersell <parkersell27@gmail.com>
2021-04-11 09:35:32 -0400

    empathy.py update

--- Empathy.py
+++ Empathy.py
@@ -3,9 +3,10 @@ import pandas as pd
 from textblob import TextBlob
 from textblob.sentiments import NaiveBayesAnalyzer
 
+
 def empathy(prompt, response):
-    #print(te.get_emotion(prompt))
-    #print(te.get_emotion(response))
+    # print(te.get_emotion(prompt))
+    # print(te.get_emotion(response))
 
     #print(TextBlob(prompt, analyzer=NaiveBayesAnalyzer()).sentiment)
     #print(TextBlob(response, analyzer=NaiveBayesAnalyzer()).sentiment)
@@ -13,9 +14,18 @@ def empathy(prompt, response):
     # Gets just the emotion values for each statement.
     promptEmotions = te.get_emotion(prompt).values()
     responseEmotions = te.get_emotion(response).values()
+    score = 5 - ((sum(pow(float(a)-float(b), 2)
+                 for a, b in zip(promptEmotions, responseEmotions)) * 8) ** (1/2))
 
+    # subtracts one from score if word length is less than 7
+    if len(response.split(" ")) < 7:
+        if score >= 2:
+            score -= 2
+        elif score < 2:
+            score = 2
     # Calculates the distance between the emotion vectors.
-    return 5 - ((sum(pow(float(a)-float(b),2) for a, b in zip(promptEmotions, responseEmotions)) * 8) ** (1/2))
+    return score
+
 
 def getConversations(n):
     train = pd.read_csv("train.csv")
@@ -33,23 +43,35 @@ def getConversations(n):
         s2id = convo["speaker_idx"].unique()[1]
         s1 = convo.loc[convo["speaker_idx"] == s1id]
         s2 = convo.loc[convo["speaker_idx"] == s2id]
-        s1words = [s.replace("_comma_", ",") for s in s1["utterance"].values.tolist()]
-        s2words = [s.replace("_comma_", ",") for s in s2["utterance"].values.tolist()]
+        s1words = [s.replace("_comma_", ",")
+                   for s in s1["utterance"].values.tolist()]
+        s2words = [s.replace("_comma_", ",")
+                   for s in s2["utterance"].values.tolist()]
         context = convo["context"].str.extract('(\w+)').values[0].item()
         escore = int(convo["empathy"].values[0])
         prompt = [convo["prompt"].unique()[0]]
         lsconv.append([context, prompt + s1words, s2words, escore])
     return lsconv
 
-def testing(n):
-    conversations = getConversations(n)
+
+def readTest(csv):
+    train = pd.read_csv(csv)
+    trainlist = train.values.tolist()
+    return trainlist
+
+
+def testing(csv):
+    conversations = readTest(csv)
     error = 0
+    count = 1
     for conversation in conversations:
-        person1 = " ".join(conversation[1])
-        person2 = " ".join(conversation[2])
-        #print(person2)
-        #print("Prompt Length: " + str(len(person1)))
-        #print("Response Length: " + str(len(person2)))
-        temp = conversation[3] - empathy(person1, person2)
+        person1 = "".join(conversation[1])
+        person2 = "".join(conversation[2])
+        # print(person2)
+        escore = empathy(person1, person2)
+
+        temp = conversation[3] - escore
+        print(count, escore, conversation[3])
+        count += 1
         error += abs(temp)
-    print(error / n)
+    print(error / len(conversations))
